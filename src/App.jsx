a656b8c292072c90363a5c15308e06818f378a91
shekhmohibur <mohibur.rahman2003@gmail.com>
2025-08-16 18:37:01 +0600

    added two more component subscribe and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
+import Footer from './footer/Footer'
 import Header from './header/Header'
 import Hero from './hero/Hero'
 import Players from './players/Players'
+import Subscription from './subscription/Subscription'
 function App() {
   return (
     <div className='max-w-7xl px-6 mx-auto mt-5'>
     <Header></Header>
     <Hero></Hero>
     <Players></Players>
+    <div>
+      <Subscription></Subscription>
+      <Footer></Footer>
+    </div>
     </div>
   )
 }
---
+++ src/subscription/Subscription.jsx
@@ -0,0 +1,14 @@
+const Subscription = () => {
+    return (
+        <div>
+            <h3>Subscribe to our Newsletter</h3>
+            <p>Get the latest updates and news right in your inbox!</p>
+            <div>
+                <input type="email" />
+                <button>Subscribe</button>
+            </div>
+        </div>
+    );
+};
+
+export default Subscription;
